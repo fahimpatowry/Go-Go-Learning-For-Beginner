62b35f8ea9b28a3736b50549a54f817c2f91845e
mdfahimpatowry <fahimjoy768@gmail.com>
2025-11-27 00:59:29 +0600

    get-api

--- main.go
+++ main.go
@@ -1,33 +1,23 @@
 package main
 
-import fmt "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
-// import "rsc.io/quote"
-import "github.com/google/shlex"
+func main(){
+	r := gin.Default() // create a router
 
-func main() {
-	// fmt.Println("Hello, World!")
-
-	// form quote package
-	// fmt.Println(quote.Go())
-	// fmt.Println(quote.Glass())
-	// fmt.Println(quote.Hello())
-	// fmt.Println(quote.Opt())
-
-	z := "one \"two\" \"three\" four"
-	numberS, err := shlex.Split(z)
-	if err != nil {
-        fmt.Println("Error:", err)
-        return
-    }
-
-	fmt.Println(numberS)
-
-	//for lop
-	if len(numberS) > 0{
-		for i := 0; i < len(numberS); i++ {
-			fmt.Println(numberS[i])
+	// simple GET API
+	r.GET("/user", func(c *gin.Context) {
+		user := map[string]interface{}{
+			"id":   1,
+			"name": "Fahim",
+			"age":  25,
 		}
-	}
+		c.JSON(http.StatusOK, user)
+	})
 
+	// run server on port 8080
+	r.Run(":8080")
 }
