c7ee6cb5a206a8960f8a19cdfa53138c1616812a
mdfahimpatowry <fahimjoy768@gmail.com>
2025-11-26 23:30:05 +0600

    first-go-run

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import fmt "fmt"
+
+import "rsc.io/quote"
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	// form quote package
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Opt())
+
+
+
+}
