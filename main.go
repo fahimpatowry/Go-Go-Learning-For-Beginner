5aba7783fc16c30ec24d4f227c47bc186f55ad5d
mdfahimpatowry <fahimjoy768@gmail.com>
2025-11-26 23:54:30 +0600

    split-by-package-print-by-for-loop

--- main.go
+++ main.go
@@ -2,17 +2,32 @@ package main
 
 import fmt "fmt"
 
-import "rsc.io/quote"
+// import "rsc.io/quote"
+import "github.com/google/shlex"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println("Hello, World!")
 
 	// form quote package
-	fmt.Println(quote.Go())
-	fmt.Println(quote.Glass())
-	fmt.Println(quote.Hello())
-	fmt.Println(quote.Opt())
+	// fmt.Println(quote.Go())
+	// fmt.Println(quote.Glass())
+	// fmt.Println(quote.Hello())
+	// fmt.Println(quote.Opt())
 
+	z := "one \"two\" \"three\" four"
+	numberS, err := shlex.Split(z)
+	if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
 
+	fmt.Println(numberS)
+
+	//for lop
+	if len(numberS) > 0{
+		for i := 0; i < len(numberS); i++ {
+			fmt.Println(numberS[i])
+		}
+	}
 
 }
