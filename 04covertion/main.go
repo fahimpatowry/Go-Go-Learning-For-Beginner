73f778eea59ede882d9404fd7ede17da48dad5ac
@fahim.patowary <fahim.patowary@technonext.com>
2026-01-14 18:12:01 +0600

    04covertion

---
+++ 04covertion/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("welcome")
+	fmt.Println("Plz rate our pizza between 1 and 5")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	input, _ := reader.ReadString('\n')
+
+	fmt.Println("input", input)
+
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(numRating + 1)
+	}
+
+}
